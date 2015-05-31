3add73800f90eb68ca3a80b5150ade708dde8123
Josep <apuratepp@gmail.com>
2015-06-01 00:02:02 +0200

    Parsed data 🗿

--- lib/meteoub_elixir.ex
+++ lib/meteoub_elixir.ex
@@ -1,10 +1,40 @@
 defmodule MeteoubElixir do
-  def fetch_data do
+  def fetch do
     HTTPotion.start
     HTTPotion.get("http://infomet.am.ub.es/campbell/www.dat")
   end
 
-  def splitted_data do
-    fetch_data.body |> String.split("\n")
+  def split do
+    fetch.body |> String.split("\n")
+  end
+
+  def parse do
+    keys = [
+      :date,
+      :time,
+      :temperature_min,
+      :temperature_max,
+      :temperature_average,
+      :humidity_min,
+      :humidity_max,
+      :humidity_average,
+      :pressure_min,
+      :pressure_max,
+      :pressure_average,
+      :wind_speed_average,
+      :wind_speed_max,
+      :solar_irradiation_since_00_utc,
+      :solar_radiation_global,
+      :solar_radiation_difusse,
+      :solar_radiation_ultraviolet,
+      :solar_azimut,
+      :solar_altitude,
+      :sunrise,
+      :sunset,
+      :rain,
+      :is_it_raining?
+    ]
+
+    Enum.zip(keys, split)
   end
 end
--- test/meteoub_elixir_test.exs
+++ test/meteoub_elixir_test.exs
@@ -5,13 +5,15 @@ defmodule MeteoubElixirTest do
 
   @tag timeout: 1_000_000
 
-  test "fetch_weather_data" do
-    response = MeteoubElixir.fetch_data
-    assert 200 == response.status_code
+  test "fetch" do
+    assert 200 == MeteoubElixir.fetch.status_code
   end
 
-  test "splitted_data" do
-    list = MeteoubElixir.splitted_data
-    assert is_list(list)
+  test "split" do
+    assert is_list(MeteoubElixir.split)
+  end
+
+  test "parse" do
+    assert is_list(MeteoubElixir.parse)
   end
 end

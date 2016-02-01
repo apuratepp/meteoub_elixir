27b279d3a30c35ef770f04c9488525cee4e2b90c
Josep <apuratepp@gmail.com>
2016-02-01 19:16:26 +0100

    Move keys to module attribute 🔦

--- lib/meteoub_elixir.ex
+++ lib/meteoub_elixir.ex
@@ -1,5 +1,30 @@
 defmodule MeteoubElixir do
   @data_url "http://infomet.am.ub.es/campbell/www.dat"
+  @keys [
+          :date,
+          :time,
+          :temperature_min,
+          :temperature_max,
+          :temperature_average,
+          :humidity_min,
+          :humidity_max,
+          :humidity_average,
+          :pressure_min,
+          :pressure_max,
+          :pressure_average,
+          :wind_speed_average,
+          :wind_speed_max,
+          :solar_irradiation_since_00_utc,
+          :solar_radiation_global,
+          :solar_radiation_difusse,
+          :solar_radiation_ultraviolet,
+          :solar_azimut,
+          :solar_altitude,
+          :sunrise,
+          :sunset,
+          :rain,
+          :is_it_raining?
+        ]
 
   def fetch do
     HTTPotion.start
@@ -11,32 +36,6 @@ defmodule MeteoubElixir do
   end
 
   def parse do
-    keys = [
-      :date,
-      :time,
-      :temperature_min,
-      :temperature_max,
-      :temperature_average,
-      :humidity_min,
-      :humidity_max,
-      :humidity_average,
-      :pressure_min,
-      :pressure_max,
-      :pressure_average,
-      :wind_speed_average,
-      :wind_speed_max,
-      :solar_irradiation_since_00_utc,
-      :solar_radiation_global,
-      :solar_radiation_difusse,
-      :solar_radiation_ultraviolet,
-      :solar_azimut,
-      :solar_altitude,
-      :sunrise,
-      :sunset,
-      :rain,
-      :is_it_raining?
-    ]
-
-    Enum.zip(keys, split)
+    Enum.zip(@keys, split)
   end
 end
